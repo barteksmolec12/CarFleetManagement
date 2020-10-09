cac990df07c8988692e0bdd3adca89c20762580b
barteksmolec12 <barteksmolec@o2.pl>
2020-10-09 23:03:03 +0200

    Edit post action - complete

--- CarFleet/Controllers/OfferController.cs
+++ CarFleet/Controllers/OfferController.cs
@@ -40,13 +40,50 @@ namespace CarFleet.Controllers
 			return View(offerModel);
 
 		}
-		public async Task <IActionResult> OffersManagment()
+		public async Task<IActionResult> OffersManagment()
 		{
 			var offers = await _db.Offers.Include(c => c.Car).ToListAsync();
 			return View(offers);
+
+
 		}
 
+		[HttpPost]
+		public async Task<IActionResult> Delete(int id)
+		{
+			
+				var offerFromDb = await _db.Offers.FindAsync(id);
+				_db.Offers.Remove(offerFromDb);
+				await _db.SaveChangesAsync();
+				return RedirectToAction(nameof(OffersManagment));
+			
+		}
+		public async Task <IActionResult> Edit(int? id)
+		{
+			if(id ==null)
+			{
+				return NotFound();
+			}
 
+			var offerFromDb = await _db.Offers.Include(m=>m.Car).FirstOrDefaultAsync(m => m.Id == id);
+			
+			return View(offerFromDb);
+		}
+		[ValidateAntiForgeryToken]
+		[HttpPost]
+		public async Task<IActionResult> Edit(Offers offerModel)
+		{
+			if (ModelState.IsValid)
+			{
+
+				//var offerFromDb = await _db.Offers.Include(s => s.Car).Where(s => s.CarId == offerModel.CarId).FirstOrDefaultAsync();
+				_db.Offers.Update(offerModel);
+				await _db.SaveChangesAsync();
+				return RedirectToAction(nameof(OffersManagment));
+			}
+			return View(offerModel);
+
+		}
 
 
 	}

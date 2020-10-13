c29bac6591321fbbb933c9dd6325c3dc3babf07a
barteksmolec12 <barteksmolec@o2.pl>
2020-10-13 23:21:32 +0200

    Create AccountView -

--- CarFleet/Controllers/AuctionController.cs
+++ CarFleet/Controllers/AuctionController.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarFleet.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarFleet.Controllers
 {
 	public class AuctionController : Controller
 	{
+		private readonly ApplicationDbContext _db;
+		public AuctionController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
+		}
+		public async Task<IActionResult> AllAuctions()
+		{
+			var offers = await _db.Offers.Include(c => c.Car).ToListAsync();
+			return View(offers);
+
+
 		}
 	}
 }
